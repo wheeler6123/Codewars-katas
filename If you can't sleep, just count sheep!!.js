8baaf67fbe8f193f9a7b3430b39f7f7ad0551cfb
John Wheeler <wheeler6123@gmail.com>
2022-03-29 16:00:38 -0600

    added new kata solution

---
+++ If you can't sleep, just count sheep!!.js
@@ -0,0 +1,9 @@
+//Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+let countSheep = function (num){
+    let text = "";
+    for (let i = 1; i <= num; i++){
+      text = text += `${i} sheep...`
+    }
+    return text;
+  }
