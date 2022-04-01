8baddea1f3479e3dd879c5b073331ce2fe033217
John Wheeler <wheeler6123@gmail.com>
2022-04-01 12:41:23 -0600

    add kata solution for Function 1 Hello World

---
+++ 8kyu-function1HelloWorld.js
@@ -0,0 +1,14 @@
+/*Make a simple function called greet that returns the most-famous "hello world!".
+
+Style Points
+Sure, this is about as easy as it gets. But how clever can you be to create the most creative hello world you can think of? What is a "hello world" solution you would want to show your friends?*/
+
+//P: this function does not require any params taken in
+//R: return 'hello world!'
+//E: function(){
+    //return 'hello world!'
+//}
+//P: declare a function
+//   function returns 'hello world!'
+
+const greet = () => 'hello world!'
