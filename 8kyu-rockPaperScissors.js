aedc74cf2b60a33d4799cb23841aceac0b0f5c16
John Wheeler <wheeler6123@gmail.com>
2022-04-06 17:24:46 -0600

    add new kata solution for Rock Paper Scissors!

---
+++ 8kyu-rockPaperScissors.js
@@ -0,0 +1,21 @@
+/*Description:
+Rock Paper Scissors
+Let's play! You have to return which player won! In case of a draw return Draw!.
+
+Examples:
+
+rps('scissors','paper') // Player 1 won!
+rps('scissors','rock') // Player 2 won!
+rps('paper','paper') // Draw!*/
+
+const rps = (p1, p2) => {
+    if ((p1 === 'rock' && p2 === 'scissors') ||
+        (p1 === 'paper' && p2 === 'rock') ||
+        (p1 === 'scissors' && p2 === 'paper')) {
+      return 'Player 1 won!';
+    } else if (p1 === p2) {
+      return 'Draw!';
+    } else {
+      return 'Player 2 won!'
+    }
+  };
