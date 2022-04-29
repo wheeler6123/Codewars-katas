a2ec0a42619a8ca99d412a763401edd118618f25
John Wheeler <wheeler6123@gmail.com>
2022-04-29 11:57:30 -0600

    add new kata solution for Calculate BMI

---
+++ 8kyu-calculateBMI.js
@@ -0,0 +1,22 @@
+/*Write function bmi that calculates body mass index (bmi = weight / height2).
+
+if bmi <= 18.5 return "Underweight"
+
+if bmi <= 25.0 return "Normal"
+
+if bmi <= 30.0 return "Overweight"
+
+if bmi > 30 return "Obese"*/
+
+function bmi(weight, height) {
+    let calculatedBmi = weight/height**2;
+    if (calculatedBmi <= 18.5){
+      return 'Underweight';
+    }else if (calculatedBmi <= 25) {
+      return 'Normal';
+    }else if(calculatedBmi <= 30){
+      return 'Overweight';
+    }else {
+      return 'Obese';
+    }
+  }
