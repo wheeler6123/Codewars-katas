ac2e3c9fbae957e53757c049299e936f6496671f
John Wheeler <wheeler6123@gmail.com>
2022-05-25 09:30:41 -0600

    add kata solution for You Cant Code Under Pressure #1

---
+++ 8kyu-youCantCodeUnderPressure1.js
@@ -0,0 +1,11 @@
+//Code as fast as you can! You need to double the integer and return it.
+
+//P: input will always be a single integer
+//R: double the input
+//E: 2 should return 4
+    //5 shoud return 10
+//P: function (n){
+    //return n*2
+//}
+
+const doubleInteger = n => 2*n;
