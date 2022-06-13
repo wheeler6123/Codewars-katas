d7a7fb37f9f028bfc63e8f12c9f234c9aeccbc7a
John Wheeler <wheeler6123@gmail.com>
2022-06-13 14:56:24 -0600

    add kata solution for Third Angle of a Triangle

---
+++ 8kyu-thirdAngleOfATriangle.js
@@ -0,0 +1,17 @@
+/*You are given two interior angles (in degrees) of a triangle.
+
+Write a function to return the 3rd.
+
+Note: only positive integers will be tested.*/
+
+
+//P: input will be two positive integers representing 2 of the 3 angles of a triangle
+//R: given the two angles provided, return the third angle
+//E: (30,60) => 90
+  //(60,60) => 60
+//P: function(a1,a2){
+  //calculate third angle
+  //return
+//}
+
+const otherAngle = (a1, a2) => 180 - a1 - a2
