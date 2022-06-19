0657b2b521092f94e17110b47ce2182cd71cc6c3
John Wheeler <wheeler6123@gmail.com>
2022-06-19 12:27:47 -0600

    add kata solution for Get Planet Name by ID

---
+++ 8kyu-getPlanetNameById.js
@@ -0,0 +1,38 @@
+/*The function is not returning the correct values. Can you figure out why?
+
+Example (Input --> Output ):
+
+3 --> "Earth"*/
+
+//function was missing break in the switch-case syntax
+
+function getPlanetName(id){
+    var name;
+    switch(id){
+      case 1:
+        name = 'Mercury'
+        break;
+      case 2:
+        name = 'Venus'
+        break;
+      case 3:
+        name = 'Earth'
+        break;
+      case 4:
+        name = 'Mars'
+        break;
+      case 5:
+        name = 'Jupiter'
+        break;
+      case 6:
+        name = 'Saturn'
+        break;
+      case 7:
+        name = 'Uranus'
+        break;
+      case 8:
+        name = 'Neptune'
+    }
+    
+    return name;
+  }
