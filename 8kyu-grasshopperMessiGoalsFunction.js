823220172a820db3cfe02c459cf6030ea7deade4
John Wheeler <wheeler6123@gmail.com>
2022-06-20 12:32:31 -0600

    add kata solution for Grasshopper Messi Goals Function

---
+++ 8kyu-grasshopperMessiGoalsFunction.js
@@ -0,0 +1,26 @@
+/*Messi goals function
+Messi is a soccer player with goals in three leagues:
+
+LaLiga
+Copa del Rey
+Champions
+Complete the function to return his total number of goals in all three leagues.
+
+Note: the input will always be valid.
+
+For example:
+
+5, 10, 2  -->  17*/
+
+
+
+
+//P: input will be 3 integers representing number of goals scored in each of 3 leagues
+//R: total goals scored across all 3 leagues
+//E: (0,0,0) => 0
+  //(20, 4, 14) => 38
+//P: function(l1, l2, l3){
+  //return all inputs added together
+//}
+
+const goals = (g1, g2, g3) => g1 + g2 + g3
