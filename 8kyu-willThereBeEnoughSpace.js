4398b628362e83517dd7cbd6ee38638e93259503
John Wheeler <wheeler6123@gmail.com>
2022-06-29 14:26:43 -0600

    add kata solution for Will There Be Enough Space

---
+++ 8kyu-willThereBeEnoughSpace.js
@@ -0,0 +1,28 @@
+/*The Story:
+Bob is working as a bus driver. However, he has become extremely popular amongst the city's residents. With so many passengers wanting to get aboard his bus, he sometimes has to face the problem of not enough space left on the bus! He wants you to write a simple program telling him if he will be able to fit all the passengers.
+
+Task Overview:
+You have to write a function that accepts three parameters:
+
+cap is the amount of people the bus can hold excluding the driver.
+on is the number of people on the bus excluding the driver.
+wait is the number of people waiting to get on to the bus excluding the driver.
+If there is enough space, return 0, and if there isn't, return the number of passengers he can't take.
+
+Usage Examples:
+cap = 10, on = 5, wait = 5 --> 0 # He can fit all 5 passengers
+cap = 100, on = 60, wait = 50 --> 10 # He can't fit 10 of the 50 waiting*/
+
+
+
+//P: three inputs, all positive numbers, representing capacity of the bus, number of current passengers, and number of passengers waiting
+//R: number of passengers who won't fit on the bus. if all passengers fit, return should be 0
+//E: (10, 5, 5) => 0
+  //(100,60,50) => 10
+//P: function (cap, on, wait){
+  //if/else statement
+  //if cap - on - wait >= 0, return 0
+  //else if cap - on - wait < 0, return the absolute value of the negative number
+//}
+
+const enough = (cap, on, wait) => cap - on - wait >= 0 ? 0 : Math.abs(cap-on-wait)
