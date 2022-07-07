57c93fb00e2b48b5e607bb1ccf30ccd09a7dd1ac
John <wheeler6123@gmail.com>
2022-07-07 13:02:48 -0600

    add kata solution for Grasshopper Check for factor

---
+++ 8kyu-grasshopperCheckForFactor.js
@@ -0,0 +1,26 @@
+/*This function should test if the factor is a factor of base.
+
+Return true if it is a factor or false if it is not.
+
+About factors
+Factors are numbers you can multiply together to get another number.
+
+2 and 3 are factors of 6 because: 2 * 3 = 6
+
+You can find a factor by dividing numbers. If the remainder is 0 then the number is a factor.
+You can use the mod operator (%) in most languages to check for a remainder
+For example 2 is not a factor of 7 because: 7 % 2 = 1
+
+Note: base is a non-negative number, factor is a positive number.*/
+
+
+
+//P: two input params, base and factor. base is a non-negative number, factor is a positive number
+//R: return a boolean value for if the factor is a factor of the base
+//E: (10,2) => 'true'
+  //  (9,2) => 'false'
+//P: function (base,factor) {
+ //return base % factor === 0
+//}
+
+const checkForFactor = (base, factor) => base % factor === 0
