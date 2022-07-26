9e720ff7bbc5bb63b68fe16477a626006dc257f4
John <wheeler6123@gmail.com>
2022-07-26 13:18:10 -0600

    add kata solution for Grasshopper Debug Say Hello

---
+++ 8kyu-grasshopperDebugSayHello.js
@@ -0,0 +1,15 @@
+/*Debugging sayHello function
+The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+Example output:
+
+Hello, Mr. Spock*/
+
+
+
+//P: input will be a string containing someone's name, no indication that it could possibly be empty
+//R: return a greeting using the input name
+//E: 'Mr. Spock' => 'Hello, Mr. Spock'
+//P: use template literal to respond with Hello, {name}
+
+const sayHello = name => `Hello, ${name}`
