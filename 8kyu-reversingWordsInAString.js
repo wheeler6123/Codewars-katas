d34079204fa61f5aace8af90359a9b21a23f1a14
John <wheeler6123@gmail.com>
2022-08-02 17:09:00 -0600

    add kata solution for Reversing Words in a String

---
+++ 8kyu-reversingWordsInAString.js
@@ -0,0 +1,21 @@
+/*You need to write a function that reverses the words in a given string. A word can also fit an empty string. If this is not clear enough, here are some examples:
+
+As the input may have trailing spaces, you will also need to ignore unneccesary whitespace.
+
+Example (Input --> Output)
+
+"Hello World" --> "World Hello"
+"Hi There." --> "There. Hi"
+Happy coding!*/
+
+
+
+//P: input will be a string of various length
+//R: return the string reversed
+//E: "Hello World" --> "World Hello"
+  // "Hi There." --> "There. Hi"
+
+
+  function reverse(string){
+    return string.split(' ').reverse().join(' ');
+  }
