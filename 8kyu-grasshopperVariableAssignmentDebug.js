e6497381fb578c5205570b369c1c8cb30a18ac76
John <wheeler6123@gmail.com>
2022-08-07 16:43:16 -0600

    add kata solution for Grasshopper Variable Assignment Debug

---
+++ 8kyu-grasshopperVariableAssignmentDebug.js
@@ -0,0 +1,16 @@
+/*Variable assignment
+Fix the variables assigments so that this code stores the string 'devLab' in the variable name.*/
+
+
+//P: input is 3 broken variable assignments
+//R: fix the variables so they are functional
+//P: here is what is provided:
+  // var a == "dev"
+  // var b == "Lab"
+  // var name == a + b
+
+
+  var a = "dev"
+  var b = "Lab"
+  
+  var name = a + b
