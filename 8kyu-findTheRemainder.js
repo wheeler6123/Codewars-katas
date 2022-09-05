7dbb8bc374c87df1907194bc49802d36f833eecd
John <wheeler6123@gmail.com>
2022-09-05 12:45:42 -0600

    add kata solution for Find The Remainder

---
+++ 8kyu-findTheRemainder.js
@@ -0,0 +1,32 @@
+/*ask:
+Write a function that accepts two integers and returns the remainder of dividing the larger value by the smaller value.
+
+Division by zero should return NaN.
+
+Examples:
+n = 17
+m = 5
+result = 2 (remainder of `17 / 5`)
+
+n = 13
+m = 72
+result = 7 (remainder of `72 / 13`)
+
+n = 0
+m = -1
+result = 0 (remainder of `0 / -1`)
+
+n = 0
+m = 1
+result - division by zero (refer to the specifications on how to handle this in your language)*/
+
+
+//P: two inputs, both integers, can be positive, negative, or 0
+//R: return the remainder of dividing the larger value by the smaller value; division by 0 should return NaN
+//E: 17, 5 = > 2
+  // 13, 72 => 7
+//P: check for a 0 input, if present return NaN
+  // if no 0 input, return the mod of the larger input mod smaller input
+
+
+  const remainder = (n,m) => n > m ? n % m : m % n
