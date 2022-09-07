775e0468b3405b408829aabbb297292634469eeb
John <wheeler6123@gmail.com>
2022-09-07 11:43:57 -0600

    add kata solution for Grasshopper Function Syntax Debugging

---
+++ 8kyu-grasshopperFunctionSyntaxDebugging.js
@@ -0,0 +1,16 @@
+/*Grasshopper - Function syntax debugging
+A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.*/
+
+
+//P: input is a broken function
+//R: fix the function so it returns the two strings joined
+//E: ['take', 'item'] => 'take item'
+  // ['use', 'sword'] => 'use sword'
+//P: this is the provided broken function:
+/* function main [verb, noun]
+  return verb + noun
+} */
+
+function main (verb, noun){
+    return verb + noun
+  }
