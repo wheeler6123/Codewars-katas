75961f2ef0aab4a8c41f13db6f5520fd98e046a4
John <wheeler6123@gmail.com>
2022-10-02 09:35:56 -0600

    add kata solution for Reverse List Order

---
+++ 8kyu-reverseListOrder.js
@@ -0,0 +1,14 @@
+/*In this kata you will create a function that takes in a list and returns a list with the reverse order.
+
+Examples (Input -> Output)
+* [1, 2, 3, 4]  -> [4, 3, 2, 1]
+* [9, 2, 0, 7]  -> [7, 0, 2, 9]*/
+
+
+//P: input will be an array, indications are that it will contain a list of integers and will not be empty
+//R: return the list of numbers in reverse order of the input
+//E: [1,2,3,4] => [4,3,2,1]
+//P: return the input using the reverse method
+
+
+const reverseList = a => a.reverse()
