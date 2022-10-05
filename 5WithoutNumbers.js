6f491ec2f14ee6f705b232a4ecfdb38b6b62c836
John <wheeler6123@gmail.com>
2022-10-05 14:00:42 -0600

    add kata solution for 5 Without Numbers

---
+++ 5WithoutNumbers.js
@@ -0,0 +1,21 @@
+/*Write a function that always returns 5
+
+Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+Good luck :)*/
+
+
+
+//P: no input params
+//R: return 5 every time, no matter the input, without using any prohibited characters
+//E: () => 5
+    // 'snow' => 5
+//P: declare var with length of 5
+    // return var.length 
+
+
+
+function unusualFive() {
+    let result = 'fiver';
+    return result.length
+  }
