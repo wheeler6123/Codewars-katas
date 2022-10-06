3a9671b8294e6eb9888d96a74dcb4f3a564667c9
John <wheeler6123@gmail.com>
2022-10-06 15:55:21 -0600

    add kata solution for Function 2 - squaring an argument

---
+++ 8kyu-function2SquaringAnArgument.js
@@ -0,0 +1,11 @@
+
+//Now you have to write a function that takes an argument and returns the square of it.
+
+
+//P: input will be an integer, no indication it could be empty
+//R: return the square of the input
+//E: 3 => 9
+//P: return n*n
+
+
+const square = n => n*n
