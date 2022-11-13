5dfe4cc6b229a5ae964a165a71375bb4b17691df
John <wheeler6123@gmail.com>
2022-11-13 12:02:26 -0700

    add kata solution for Sort Arrays-1

---
+++ 7kyu-sortArrays.js
@@ -0,0 +1,9 @@
+//Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+
+
+//P: one input, an array containing strings
+//R: return the array sorted into lexographical order
+//E: ['one', 'two', 'three'] => ['one', 'three', 'two']
+//P: use built in sort method
+
+const sortme = arr => arr.sort()
