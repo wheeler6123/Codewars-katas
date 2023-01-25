f27a599a3041f92ba5a45834e15d4c9ce4401505
John <wheeler6123@gmail.com>
2023-01-25 13:03:15 -0700

    add kata solution for speedcode array madness

---
+++ 8kyu-speedcodeArrayMadness.js
@@ -0,0 +1,21 @@
+/* 
+SpeedCode #2 - Array Madness
+Objective
+Given two integer arrays a, b, both of length >= 1, create a program that returns true if the sum of the squares of each element in a is strictly greater than the sum of the cubes of each element in b.
+
+E.g.
+
+arrayMadness([4, 5, 6], [1, 2, 3]); // returns true since 4 ** 2 + 5 ** 2 + 6 ** 2 > 1 ** 3 + 2 ** 3 + 3 ** 3
+Get your timer out. Are you ready? Ready, get set, GO!!!
+*/
+
+
+
+//P: two inputs, both arrays containing positive integers
+//R: return a boolean value true or false for if the sum of the squares in array a is greater than the sum of the cubes in array b
+//E: [4,5,6], [1,2,3] => true
+//P: first, get the sum of the squares in array 1 and sum of the cubes in array b, return the former > the latter as a boolean
+
+
+
+const arrayMadness = (a,b) => a.reduce((a,b) => a + b**2, 0) > b.reduce((a,b) => a + b**3, 0)
