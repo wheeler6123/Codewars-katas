8e5a2df14c0be059ec8d7e6c4480acb89f0a0811
John <wheeler6123@gmail.com>
2023-01-31 14:58:08 -0700

    add kata solution for incorrect division method

---
+++ 8kyu-incorrectDivisionMethod.js
@@ -0,0 +1,13 @@
+/* 
+This method, which is supposed to return the result of dividing its first argument by its second, isn't always returning correct values. Fix it.
+*/
+
+
+
+//P: input is a broken function
+//R: fix the function so that it correctly returns the division of x and y
+//E: 6,2 => 3
+//P: given broken function uses x//y when only 1 / is needed
+
+
+const solve = (x, y) => x / y
