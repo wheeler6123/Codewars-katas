621406eaa07f44cfec3c7e0f509f9ac9bd67336a
John <wheeler6123@gmail.com>
2023-10-03 16:27:04 -0600

    add ts solution for 8kyu Multiply

---
+++ typescript/8kyu-multiply.ts
@@ -0,0 +1,13 @@
+/* This code does not execute properly. Try to figure out why.
+
+ input code: export function multiply(a: number, b: number): number {
+  return a + b;
+}
+
+*/
+
+// Solution
+
+export function multiply(a: number, b: number): number {
+  return a * b;
+}
