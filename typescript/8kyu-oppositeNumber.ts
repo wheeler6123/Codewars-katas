ad86c3bedf9596daf1129f239c190d6a23488e20
John <wheeler6123@gmail.com>
2023-10-13 15:43:34 -0600

    add TS kata solution for opposite number

---
+++ typescript/8kyu-oppositeNumber.ts
@@ -0,0 +1,18 @@
+/*
+Very simple, given an integer or a floating-point number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+
+*/
+
+export class Kata {
+    static opposite(n: number) {
+
+        return -n;
+
+    }
+}
