4cb18d92930b01bbc4571f727bc3843ecc2dfadf
John <wheeler6123@gmail.com>
2023-10-14 15:14:11 -0600

    add ts solution for square every digit

---
+++ typescript/7kyu-squareEveryDigit.ts
@@ -0,0 +1,24 @@
+// Welcome.In this kata, you are asked to square every digit of a number and concatenate them.
+
+// For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.(81 - 1 - 1 - 81)
+
+// Example #2: An input of 765 will / should return 493625 because 72 is 49, 62 is 36, and 52 is 25.(49 - 36 - 25)
+
+// Note: The function accepts an integer and returns an integer.
+
+// Happy Coding!
+
+export class Kata {
+    static squareDigits(num: number): number {
+        let nums = num.toString().split('');
+        let numStr = '';
+
+        for (let i = 0; i < nums.length; i++) {
+            let currNum = nums[i];
+
+            numStr += (+currNum * +currNum);
+        }
+
+        return +numStr;
+    }
+}
