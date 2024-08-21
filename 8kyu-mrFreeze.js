49096faf98938cf23c1c5d3bfac699bf7b48d4d5
John <wheeler6123@gmail.com>
2024-08-21 13:55:45 -0400

    add solution for MrFreeze

---
+++ 8kyu-mrFreeze.js
@@ -0,0 +1,5 @@
+// There is an object/class already created called MrFreeze. Mark this object as frozen so that no other changes can be made to it.
+
+// mark the MrFreeze object instance as frozen
+
+Object.freeze(MrFreeze);
